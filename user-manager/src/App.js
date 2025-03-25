46797c3c9c45699cf3a3b376dcb1e8b4fcedd917
Kishore-67 <kishore762k4@gmail.com>
2025-03-25 11:47:52 +0530

    first commit

---
+++ user-manager/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '../src/Components/Nav'
+import Nav from '../src/Components/Nav';
+function App() {
+  return (
+    <div className="App">
+      
+        <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-manager/src/Components/Nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+        <header>
+            <h1>User Management</h1>
+            <button>
+                Menu
+            </button>
+        </header>
+    );
+}
+
+export default Nav;
